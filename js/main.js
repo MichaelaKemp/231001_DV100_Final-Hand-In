f7c3903d07403583d6b55537f6f7df672a906b83
MichaelaKemp <231001@virtualwindow.co.za>
2023-06-09 23:21:03 +0200

    Progress Milestone Two
    Added more info.

---
+++ js/main.js
@@ -0,0 +1,43 @@
+subSubmit = () => 
+{
+
+const hashMap = new Map();
+hashMap.set("Rye", 4);
+hashMap.set("Ciabatta", 2);
+hashMap.set("Grain", 3);
+hashMap.set("Baguette", 6);
+hashMap.set("Sourdough", 6);
+hashMap.set("White", 6);
+hashMap.set("Cheddar Cheese", 6);
+hashMap.set("BTomatoes", 6);
+hashMap.set("Ham", 6);
+hashMap.set("Olives", 6);
+hashMap.set("Lettuce", 6);
+hashMap.set("Avocado", 6);
+hashMap.set("Bacon", 6);
+hashMap.set("Cucumber", 6);
+hashMap.set("Gherkins", 6);
+hashMap.set("Onion", 6);
+hashMap.set("Chicken Breast", 6);
+hashMap.set("Egg", 6);
+hashMap.set("Bell Pepper", 6);
+hashMap.set("Jalapenos", 6);
+hashMap.set("Red Cabbage", 6);
+hashMap.set("Sun Dried Tomatoes", 6);
+hashMap.set("Sliced Turkey", 6);
+hashMap.set("Mushrooms", 6);
+hashMap.set("Basil", 6);
+hashMap.set("Carrots", 6);
+hashMap.set("Steak", 6);
+hashMap.set("Mayonnaise", 6);
+hashMap.set("Sweet Chilli Sauce", 6);
+hashMap.set("Mustard", 6);
+hashMap.set("Sriracha", 6);
+hashMap.set("BBQ Sauce", 6);
+hashMap.set("Tomato Sauce", 6);
+console.log(hashMap);
+
+const value1 = hashMap.get("Egg");
+console.log(value1)
+
+}
